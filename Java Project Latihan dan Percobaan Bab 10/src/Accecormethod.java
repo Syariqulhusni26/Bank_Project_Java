3784a441412fe1a78ee758891d14d2d9bcf520c3
Syariqul Husni <syariqulhusni26@gmail.com>
2023-10-08 10:54:10 +0700

    Ada file baru bab sepuluh java

---
+++ Java Project Latihan dan Percobaan Bab 10/src/Accecormethod.java
@@ -0,0 +1,19 @@
+public class Accecormethod {
+        public class StudentRecord {
+            
+            private String name;
+            private String address;
+            private int age;
+            private double mathGrade;
+            private double englishGrade;
+            private double scienceGrade;
+            private double average;
+
+            public double getAverage() {
+                double result = 0;
+                result = (mathGrade+englishGrade+scienceGrade) / 3;
+
+                return result;
+            }
+        }
+}
